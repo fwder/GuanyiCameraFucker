399d83429fb62e89d2469b2ca025c0c778ab84ac
Weclont <108498345+fwder@users.noreply.github.com>
2022-07-14 03:48:43 +0800

    Update GuanyiCameraFucker.py

--- GuanyiCameraFucker.py
+++ GuanyiCameraFucker.py
@@ -97,12 +97,14 @@ def check_connection(test_connection_rtsp_url):
         closed_connection_time = int(time.time())
         temp_text = f"连接在 `{str(datetime.datetime.now())}` 时断开，请检查网络\n"
         is_connected_text += temp_text
-        mixprint(temp_text.replace('\n', ''), 'WARN', 'Markdown')
+        if forwarding_video:
+            mixprint(temp_text.replace('\n', ''), 'WARN', 'Markdown')
     elif isOpened != is_connected and isOpened:
         is_connected = isOpened
         temp_text = f"连接在 `{str(datetime.datetime.now())}` 时恢复，宕机时间：{str(int(time.time()) - closed_connection_time)}秒\n"
         is_connected_text += temp_text
-        mixprint(temp_text.replace('\n', ''), 'INFO', 'Markdown')
+        if forwarding_video:
+            mixprint(temp_text.replace('\n', ''), 'INFO', 'Markdown')
     return isOpened
 
 
@@ -167,6 +169,24 @@ def check_file_exist(rtsp_addr, uuid_name):
                 time.sleep(1)
 
 
+def check_time_to_run_code():
+    global forwarding_video
+    while True:
+        global start_time
+        global stop_time
+        start_time_h, start_time_m = start_time
+        stop_time_h, stop_time_m = stop_time
+        if str(datetime.datetime.now().hour) == str(int(start_time_h)) and str(datetime.datetime.now().minute) == str(
+                int(start_time_m)):
+            mixprint("#GCF机器人开始回放上传\nGCF 机器人 - 在 " + start_time_h + ":" + start_time_m + " 由设定的时间自动开始回放上传.")
+            forwarding_video = True
+        elif str(datetime.datetime.now().hour) == str(int(stop_time_h)) and str(datetime.datetime.now().minute) == str(
+                int(stop_time_m)):
+            mixprint("#GCF机器人停止回放上传\nGCF 机器人 - 在 " + start_time_h + ":" + start_time_m + " 由设定的时间自动停止回放上传.")
+            forwarding_video = False
+        time.sleep(59)
+
+
 def mixprint(msg, state='INFO', parse_mode=None):
     logprint(msg, state)
     dispatcher.bot.send_message(chat_id=tg_chat_id, text=msg, parse_mode=parse_mode)
@@ -178,7 +198,7 @@ def mixreplyprint(update, msg, state='INFO', parse_mode=None):
 
 
 def logprint(msg, state='INFO'):
-    print_build_msg = f"{str(datetime.datetime.now())} [{state}] " + msg.replace('\n', ' ')
+    print_build_msg = f"[{str(datetime.datetime.now())}] [{state}] " + msg.replace('\n', ' ')
     print(print_build_msg)
 
 
@@ -210,23 +230,24 @@ text: `{update.message['text']}`
 def helpness(update: Update, context):
     if authForUser(update):
         return
-    help_text = """
+    mixreplyprint(update, msg="""
 欢迎使用 GCF 机器人!
 
 以下是详细命令列表:  
-/start               开始
-/search              搜索历史回放
-/checkupload         检查当前上传任务量和运行情况
-/checkdisconnected   检查监控是否掉线
-/recorddisconnected  监控掉线记录
-/setmaxtasksnum      设置最大上传线程数
-/switchforwardingvideo 开关视频转发
-/restart             重启 GCF 机器人
-/shutdown            关闭 GCF 机器人
-/help                帮助列表
-/cancel              取消当前操作
-    """
-    mixreplyprint(update, help_text)
+/start 开始
+/search 搜索历史回放
+/isonline 查看当前在线状态
+/runstatus 查看当前运行状态
+/offlinetext 查看掉线记录
+/setstate 切换视频转发状态
+/setmaxthread 设置最大上传线程数
+/setruntime 定时开启回放上传
+/setrtspaddr 配置rtsp设备地址
+/restart 重启 GCF
+/shutdown 关闭 GCF
+/help 帮助列表
+/cancel 取消当前操作
+    """)
 
 
 def start(update: Update, context):
@@ -256,31 +277,66 @@ def search(update: Update, context):
     """)
 
 
-def checkupload(update: Update, context):
-    if authForUser(update):
-        return
-    sent_before = net_io_counters().bytes_sent  # 已发送的流量
-    recv_before = net_io_counters().bytes_recv  # 已接收的流量
-    time.sleep(1)
+def runstatus(update: Update, context):
+    global runstatus_output
+    runstatus_output = True
+    mixreplyprint(update, f"开始输出运行状态，发送 /cancel 或者任意消息来停止输出.")
+    threading.Thread(name=str(uuid.uuid4()) + "-runstatus_thread", target=runstatus_thread,
+                     args=(update.message['chat']['id'],
+                           dispatcher.bot.send_message(chat_id=update.message['chat']['id'], text=server_msg_text(),
+                                                       parse_mode='Markdown').message_id)).start()
+    return 1
+
+
+def runstatus1(update: Update, context):
+    global runstatus_output
+    runstatus_output = False
+    mixreplyprint(update, f"成功停止输出")
+    return ConversationHandler.END
+
+
+def runstatus_thread(chat_id, message_id):
+    global runstatus_output
+    while True:
+        if runstatus_output:
+            bot.edit_message_text(text=server_msg_text(), chat_id=chat_id, message_id=message_id, parse_mode='Markdown')
+        else:
+            return
+
+
+def server_msg_text():
+    sent_before = net_io_counters().bytes_sent
+    recv_before = net_io_counters().bytes_recv
+    time.sleep(0.1)
     sent_now = net_io_counters().bytes_sent
     recv_now = net_io_counters().bytes_recv
-    sent = (sent_now - sent_before) / 1024  # 算出1秒后的差值
-    recv = (recv_now - recv_before) / 1024
-    mixreplyprint(update, f"""
-任务上传情况：
+    sent = (sent_now - sent_before) / 0.1 / 1024
+    recv = (recv_now - recv_before) / 0.1 / 1024
+    list_temp = open("/proc/loadavg").read().split(' ')
+    s1min = list_temp[0]
+    s5min = list_temp[1]
+    s15min = list_temp[2]
+    text = f"""
+*任务上传情况*
+当前连接状态：{'已连接' if is_connected else '未连接'}
+视频转发状态：{'正在转发' if forwarding_video else '已停止转发'}
 正在上传的任务数：{str(task_in_progress_num)}
 在队列中等待的任务数：{str(task_in_queue_num)}
 最大同时上传任务数：{str(max_tasks_num)}
+当前转发设备数：{str(len(run_devices))}
 
-服务器运行情况：
+*服务器运行情况*
+负载：{s1min} {s5min} {s15min}
 CPU占用：{cpu_percent(interval=1)}%
-内存占用：{virtual_memory()[2]}%
+内存占用：{str(virtual_memory().percent)}%  {str(int((virtual_memory().total - virtual_memory().available) / 1024 / 1024))}M/{str(int(virtual_memory().total / 1024 / 1024))}M
+Swap占用：{str(swap_memory().percent)}%  {str(int(swap_memory().used / 1024 / 1024))}M/{str(int(swap_memory().total / 1024 / 1024))}M
 上传速度：{"{0}KB/s".format("%.2f" % sent)}
 下载速度：{"{0}KB/s".format("%.2f" % recv)}
-    """)
+    """
+    return text
 
 
-def checkdisconnected(update: Update, context):
+def isonline(update: Update, context):
     if authForUser(update):
         return
     if is_connected:
@@ -290,7 +346,7 @@ def checkdisconnected(update: Update, context):
     mixreplyprint(update, f"当前连接状态：{text_temp}")
 
 
-def recorddisconnected(update: Update, context):
+def offlinetext(update: Update, context):
     if authForUser(update):
         return
     if is_connected_text == '':
@@ -299,14 +355,14 @@ def recorddisconnected(update: Update, context):
     mixreplyprint(update, "有以下离线记录：\n" + is_connected_text, 'INFO', 'Markdown')
 
 
-def setmaxtasksnum(update: Update, context):
+def setmaxthread(update: Update, context):
     if authForUser(update):
-        return
+        return ConversationHandler.END
     mixreplyprint(update, "请输入你要设置的最大上传线程数.")
     return 1
 
 
-def setmaxtasksnum1(update: Update, context):
+def setmaxthread1(update: Update, context):
     try:
         temp_num = int(update.message.text)
     except:
@@ -318,16 +374,91 @@ def setmaxtasksnum1(update: Update, context):
     return ConversationHandler.END
 
 
-def switchforwardingvideo(update: Update, context):
+def setstate(update: Update, context):
     if authForUser(update):
         return
     global forwarding_video
     if forwarding_video:
         forwarding_video = False
-        mixreplyprint(update, '成功关闭转发.')
+        os.system("kill -9 `ps -ef| grep ffmpeg | awk '{print $2}'`")
+        mixreplyprint(update, '成功关闭转发并关闭了ffmpeg拉流.')
     else:
         forwarding_video = True
         mixreplyprint(update, '成功开启转发.')
+
+
+def setrtspaddr(update: Update, context):
+    if authForUser(update):
+        return ConversationHandler.END
+    mixreplyprint(update, "发送 /add 添加设备，发送 /delete 删除设备，发送 /cancel 或其他内容取消操作.")
+    return 1
+
+
+def addrtspdevice1(update: Update, context):
+    mixreplyprint(update, "请发送你要添加的rtsp设备地址（支持多行），添加完成后会自动重启机器人.")
+    return 2
+
+
+def addrtspdevice2(update: Update, context):
+    if not 'rtsp://' in update.message.text:
+        mixreplyprint(update, '你的输入可能不是rtsp地址，请检查其可用性，使用 /cancel 退出当前操作.')
+        return 2
+    file_text = open("rtsp.txt").read()
+    file_text += update.message.text + '\n'
+    file_text = file_text.replace('\n\n', '\n')
+    open("rtsp.txt", 'w').write(file_text)
+    mixreplyprint(update, "修改成功\n当前rtsp设备列表：\n" + file_text + "准备重启机器人...")
+    restart_for_tg1(update, context)
+
+
+def deletertspdevice1(update: Update, context):
+    text_temp = '当前rtsp设备列表：\n'
+    for iop in open("rtsp.txt"):
+        text_temp += '`' + iop + '`'
+    text_temp += "\n请发送你要删除的设备（支持多行），删除完成后会自动重启机器人."
+    mixreplyprint(update, text_temp, parse_mode='Markdown')
+    return 3
+
+
+def deletertspdevice2(update: Update, context):
+    if not 'rtsp://' in update.message.text:
+        mixreplyprint(update, '你的输入可能不是rtsp地址，请检查其可用性，使用 /cancel 退出当前操作.')
+        return 3
+    file_text = open("rtsp.txt").read()
+    file_text = file_text.replace(update.message.text, '')
+    open("rtsp.txt", 'w').write(file_text)
+    text_temp = '修改成功\n当前rtsp设备列表：\n'
+    for iop in open("rtsp.txt"):
+        text_temp += '`' + iop + '`'
+    text_temp += "准备重启机器人..."
+    mixreplyprint(update, text_temp, parse_mode='Markdown')
+    restart_for_tg1(update, context)
+
+
+def setruntime(update: Update, context):
+    if authForUser(update):
+        return ConversationHandler.END
+    start_time_h, start_time_m = start_time
+    stop_time_h, stop_time_m = stop_time
+    mixreplyprint(update,
+                  f'当前设置：\n`{str(start_time_h)}:{str(start_time_m)} {str(stop_time_h)}:{str(stop_time_m)}`\n请输入启动时间和停止时间\n例：`4:30 23:15`',
+                  parse_mode='Markdown')
+    return 1
+
+
+def setruntime1(update: Update, context):
+    global start_time
+    global stop_time
+    try:
+        list_temp = update.message.text.split(' ')[0].split(':')
+        start_time = (list_temp[0], list_temp[1])
+        list_temp = update.message.text.split(' ')[1].split(':')
+        stop_time = (list_temp[0], list_temp[1])
+    except:
+        mixreplyprint(update, '修改错误，请重新输入！')
+        return 1
+    mixreplyprint(update, '修改完成')
+    return ConversationHandler.END
 
 
 def cancel(update: Update, context):
@@ -337,8 +468,8 @@ def cancel(update: Update, context):
 
 def restart_for_tg(update: Update, context):
     if authForUser(update):
-        return
-    mixreplyprint(update, "你确定要重启机器人吗？\n发送 /yes 确定重启，发送其他任意文字取消重启.")
+        return ConversationHandler.END
+    mixreplyprint(update, "你确定要重启机器人吗？\n发送 /yes 确定，发送 /cancel 或其他任意文字取消.")
     return 1
 
 
@@ -349,7 +480,10 @@ def restart_for_tg1(update: Update, context):
             shutil.rmtree(ud + f'/')
         except:
             continue
+    mixreplyprint(update, "GCF 机器人 - 删除临时目录")
     os.system("kill -9 `ps -ef| grep ffmpeg | awk '{print $2}'`")
+    mixreplyprint(update, "GCF 机器人 - 关闭ffmpeg")
+    mixreplyprint(update, "GCF 机器人 - 重启成功")
     bot_restart_python_dir = sys.executable
     os.execl(bot_restart_python_dir, bot_restart_python_dir, *sys.argv)
 
@@ -357,36 +491,45 @@ def restart_for_tg1(update: Update, context):
 def shutdown_for_tg(update: Update, context):
     if authForUser(update):
         return
-    mixreplyprint(update, "你确定要关闭机器人吗？\n发送 /yes 确定关闭，发送其他任意文字取消关闭.")
+    mixreplyprint(update, "你确定要关闭机器人吗？\n发送 /yes 确定，发送 /cancel 或其他任意文字取消.")
     return 1
 
 
 def shutdown_for_tg1(update: Update, context):
-    mixreplyprint(update, "GCF 机器人 - 正在退出")
+    mixreplyprint(update, "GCF 机器人 - 正在关闭")
     for ud in all_uuid_dirs:
         try:
             shutil.rmtree(ud + f'/')
         except:
             continue
+    mixreplyprint(update, "GCF 机器人 - 删除临时目录")
     os.system("kill -9 `ps -ef| grep ffmpeg | awk '{print $2}'`")
+    mixreplyprint(update, "GCF 机器人 - 关闭ffmpeg")
+    mixreplyprint(update, "GCF 机器人 - 关闭成功")
+    os.system("kill -9 `ps -ef| grep GuanyiCameraFucker.py | awk '{print $2}'`")
     os.system("kill -9 `ps -ef| grep GuanyiCameraFucker.py | awk '{print $2}'`")
 
 
 @atexit.register
 def shutdown():
-    mixprint("GCF 机器人 - 正在退出")
+    mixprint("GCF 机器人 - 正在关闭")
     for ud in all_uuid_dirs:
         try:
             shutil.rmtree(ud + f'/')
         except:
             continue
+    mixprint("GCF 机器人 - 删除临时目录")
     os.system("kill -9 `ps -ef| grep ffmpeg | awk '{print $2}'`")
+    mixprint("GCF 机器人 - 关闭ffmpeg")
+    mixprint("GCF 机器人 - 关闭成功")
+    os.system("kill -9 `ps -ef| grep GuanyiCameraFucker.py | awk '{print $2}'`")
     os.system("kill -9 `ps -ef| grep GuanyiCameraFucker.py | awk '{print $2}'`")
 
 
 if __name__ == '__main__':
 
     # 初始化全局变量
+    runstatus_output = True
     is_connected = True
     forwarding_video = True
     is_connected_text = ''
@@ -400,7 +543,8 @@ if __name__ == '__main__':
     try:
         open('rtsp.txt')
     except:
-        logprint("请先在rtsp.txt中输入rtsp地址和设备名称!")
+        logprint("请创建rtsp.txt并输入rtsp地址和设备名称!")
+        exit(0)
 
     config_file = open('/root/GuanyiCameraFucker/config.yaml', 'r', encoding='utf-8').read()
     config = yaml.safe_load(config_file)
@@ -411,6 +555,8 @@ if __name__ == '__main__':
     tg_bot_token = config['tg_bot_token']
     proxy_url = config['proxy_url']
     max_tasks_num = int(config['max_tasks_num'])
+    start_time = (config['start_time'].split(':')[0], config['start_time'].split(':')[1])
+    stop_time = (config['stop_time'].split(':')[0], config['stop_time'].split(':')[1])
 
     if proxy_url == '':
         updater = Updater(token=tg_bot_token)
@@ -420,28 +566,53 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('search', search))
-    dispatcher.add_handler(CommandHandler('checkupload', checkupload))
-    dispatcher.add_handler(CommandHandler('checkdisconnected', checkdisconnected))
-    dispatcher.add_handler(CommandHandler('recorddisconnected', recorddisconnected))
     dispatcher.add_handler(ConversationHandler(
-        entry_points=[CommandHandler('setmaxtasksnum', setmaxtasksnum)],
+        entry_points=[CommandHandler('runstatus', runstatus)],
         states={
-            1: [CommandHandler('cancel', cancel), MessageHandler(Filters.text, setmaxtasksnum1)],
+            1: [MessageHandler(Filters.text, runstatus1)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     ))
-    dispatcher.add_handler(CommandHandler('switchforwardingvideo', switchforwardingvideo))
+    dispatcher.add_handler(CommandHandler('isonline', isonline))
+    dispatcher.add_handler(CommandHandler('offlinetext', offlinetext))
+    dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('setmaxthread', setmaxthread)],
+        states={
+            1: [CommandHandler('cancel', cancel), MessageHandler(Filters.text, setmaxthread1)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    ))
+    dispatcher.add_handler(CommandHandler('setstate', setstate))
+    dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('setrtspaddr', setrtspaddr)],
+        states={
+            1: [CommandHandler('add', addrtspdevice1), CommandHandler('delete', deletertspdevice1),
+                CommandHandler('cancel', cancel), MessageHandler(Filters.text, cancel)],
+            2: [CommandHandler('cancel', cancel), MessageHandler(Filters.text, addrtspdevice2)],
+            3: [CommandHandler('cancel', cancel), MessageHandler(Filters.text, deletertspdevice2)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    ))
+    dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('setruntime', setruntime)],
+        states={
+            1: [CommandHandler('cancel', cancel), MessageHandler(Filters.text, setruntime1)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    ))
     dispatcher.add_handler(ConversationHandler(
         entry_points=[CommandHandler('restart', restart_for_tg)],
         states={
-            1: [CommandHandler('yes', restart_for_tg1), MessageHandler(Filters.text, cancel)],
+            1: [CommandHandler('yes', restart_for_tg1), CommandHandler('cancel', cancel),
+                MessageHandler(Filters.text, cancel)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     ))
     dispatcher.add_handler(ConversationHandler(
         entry_points=[CommandHandler('shutdown', shutdown_for_tg)],
         states={
-            1: [CommandHandler('yes', shutdown_for_tg1), MessageHandler(Filters.text, cancel)],
+            1: [CommandHandler('yes', shutdown_for_tg1), CommandHandler('cancel', cancel),
+                MessageHandler(Filters.text, cancel)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     ))
@@ -457,13 +628,15 @@ if __name__ == '__main__':
     commands = [
         ("start", "开始"),
         ("search", "搜索历史回放"),
-        ("checkupload", "检查当前上传任务量和运行情况"),
-        ("checkdisconnected", "检查监控是否掉线"),
-        ("recorddisconnected", "监控掉线记录"),
-        ("setmaxtasksnum", "设置最大上传线程数"),
-        ("switchforwardingvideo", "开关视频转发"),
-        ("restart", "重启 GCF 机器人"),
-        ("shutdown", "关闭 GCF 机器人"),
+        ("isonline", "查看当前在线状态"),
+        ("runstatus", "查看当前运行状态"),
+        ("offlinetext", "查看掉线记录"),
+        ("setstate", "切换视频转发状态"),
+        ("setmaxthread", "设置最大上传线程数"),
+        ("setruntime", "设置运行时段"),
+        ("setrtspaddr", "配置rtsp设备地址"),
+        ("restart", "重启 GCF"),
+        ("shutdown", "关闭 GCF"),
         ("help", "帮助列表"),
         ("cancel", "取消当前操作"),
     ]
@@ -471,6 +644,7 @@ if __name__ == '__main__':
 
     i = 0
     run_devices = []
+    threading.Thread(name="check_time_to_run_code", target=check_time_to_run_code).start()
     threading.Thread(name="queue_to_upload_video", target=queue_to_upload_video).start()
     for li in open('rtsp.txt'):
         if li == '':
